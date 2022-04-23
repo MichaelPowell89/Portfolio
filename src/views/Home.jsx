e071be3fabefbbde413d5efa5f290e18a0a58970
MichaelPowell89 <61531405+MichaelPowell89@users.noreply.github.com>
2022-04-23 10:20:39 +1000

    commit

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
+import gameDesign from "../public/images/controller.png"
+import website from "../public/images/website.png"
 
 function Home (){
     return (
@@ -7,12 +9,12 @@ function Home (){
             <div className="homePage">
                 <div className="homeContainer" >
                 <a href="/gamedesign">
-                   <img className="homeImage" src="./images/controller.png"/>
+                   <img className="homeImage" src={gameDesign}/>
                    </a>
                 </div>
                 <div className="homeContainer">
                 <a href="/webdesign">
-                    <img className="homeImage" src="./images/website.png"/>
+                    <img className="homeImage" src={website}/>
                     </a>
                 </div>
             </div>
