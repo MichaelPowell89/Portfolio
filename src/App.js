19c00adafe3c914a8e0e4d81dad67ebc13128ec0
MichaelPowell89 <61531405+MichaelPowell89@users.noreply.github.com>
2022-04-23 10:49:05 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar.jsx";
 import Home from "./views/Home.jsx"
 import Game from "./views/Game.jsx"
-import Web from "./views/Web.jsx"
+// import Web from "./views/Web.jsx"
 import About from './views/About.jsx';
 import Contact from './views/Contact.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -18,7 +18,7 @@ function App() {
           <Routes>
             <Route path="/portfolio_github/" element={<Home/>}/>
             <Route path="/portfolio_github/gamedesign" element={<Game/>}/>
-            <Route path="/portfolio_github/webdesign" element={<Web/>}/>
+            {/* <Route path="/portfolio_github/webdesign" element={<Web/>}/> */}
             <Route path="/portfolio_github/About" element={<About/>}/>
             <Route path="/portfolio_github/Contact" element={<Contact/>}/>
           </Routes>
