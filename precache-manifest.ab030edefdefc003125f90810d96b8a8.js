f5a3679633432025e335dc15534f0c3102c98f79
MichaelPowell89 <61531405+MichaelPowell89@users.noreply.github.com>
2022-04-23 17:53:21 +1000

    Updates

---
+++ precache-manifest.ab030edefdefc003125f90810d96b8a8.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c6ffab2f8817646a81e",
+    "url": "/portfolio_github/static/css/main.376f13a4.chunk.css"
+  },
+  {
+    "revision": "4c6ffab2f8817646a81e",
+    "url": "/portfolio_github/static/js/main.4c6ffab2.chunk.js"
+  },
+  {
+    "revision": "45789c75afb661050078",
+    "url": "/portfolio_github/static/js/1.45789c75.chunk.js"
+  },
+  {
+    "revision": "1035962452860ac5d535",
+    "url": "/portfolio_github/static/css/2.af196464.chunk.css"
+  },
+  {
+    "revision": "1035962452860ac5d535",
+    "url": "/portfolio_github/static/js/2.10359624.chunk.js"
+  },
+  {
+    "revision": "6bb4fc6b18cbf7cac5be",
+    "url": "/portfolio_github/static/js/runtime~main.6bb4fc6b.js"
+  },
+  {
+    "revision": "53f41cbb249858138d0b6c79e2ce7915",
+    "url": "/portfolio_github/static/media/controller.53f41cbb.png"
+  },
+  {
+    "revision": "88fccf04af5dcbe78b90387c7a019b75",
+    "url": "/portfolio_github/static/media/Xbox.88fccf04.png"
+  },
+  {
+    "revision": "30faa35024eb00c9b3ad62a7a39d68f9",
+    "url": "/portfolio_github/static/media/Steam.30faa350.png"
+  },
+  {
+    "revision": "e2532f0a5cc0bee8ca3679ef3b150ca7",
+    "url": "/portfolio_github/static/media/websiteWhite.e2532f0a.png"
+  },
+  {
+    "revision": "47305402a5e102588c9946a65939438a",
+    "url": "/portfolio_github/static/media/man.47305402.png"
+  },
+  {
+    "revision": "26c94a3d5ec822e6d180ec22009cfb1f",
+    "url": "/portfolio_github/static/media/email.26c94a3d.png"
+  },
+  {
+    "revision": "17a610574ae6c3708bd9b8499310c19b",
+    "url": "/portfolio_github/static/media/CharlieCameron.17a61057.png"
+  },
+  {
+    "revision": "0046c2c2df1311f73dc9e32e7f7542df",
+    "url": "/portfolio_github/static/media/HighresScreenshot00204.0046c2c2.png"
+  },
+  {
+    "revision": "78ed6b79e8b78f5d53a60019a72cde5f",
+    "url": "/portfolio_github/static/media/HighresScreenshot00206.78ed6b79.png"
+  },
+  {
+    "revision": "f11698773da6b1e50f95ece3f58cee58",
+    "url": "/portfolio_github/static/media/HighresScreenshot00216.f1169877.png"
+  },
+  {
+    "revision": "5d5229f0a98fd106a03fab425200b3be",
+    "url": "/portfolio_github/static/media/HighresScreenshot00218.5d5229f0.png"
+  },
+  {
+    "revision": "b17664936d2fd2ec37f3b89a00180bad",
+    "url": "/portfolio_github/static/media/HighresScreenshot00509.b1766493.png"
+  },
+  {
+    "revision": "24af06be1bf15850a9caae9bf4f683fa",
+    "url": "/portfolio_github/static/media/HighresScreenshot00510.24af06be.png"
+  },
+  {
+    "revision": "76c3aefbae1be5f4fe73a38daa2bcd86",
+    "url": "/portfolio_github/static/media/HighresScreenshot00523.76c3aefb.png"
+  },
+  {
+    "revision": "76d963fa11ba279fa4ab76223920c088",
+    "url": "/portfolio_github/static/media/HighresScreenshot00526.76d963fa.png"
+  },
+  {
+    "revision": "74eb8356adca1961d47b8f6687ca8697",
+    "url": "/portfolio_github/static/media/HighresScreenshot00219.74eb8356.png"
+  },
+  {
+    "revision": "6af8a012de1b5275743a5182c8c3654b",
+    "url": "/portfolio_github/static/media/appleWhite.6af8a012.png"
+  },
+  {
+    "revision": "356d05c297ed031f7db6ca914c04ea2a",
+    "url": "/portfolio_github/static/media/LogoAsset.356d05c2.png"
+  },
+  {
+    "revision": "a90d10588625adec0f7c5a4f75b48298",
+    "url": "/portfolio_github/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio_github/precache-manifest.7391526946f66f9fb3bb7e815abd858a.js"
+  "/portfolio_github/precache-manifest.ab030edefdefc003125f90810d96b8a8.js"
 );
 
 workbox.clientsClaim();
