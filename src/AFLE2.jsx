b2c13e6297f10700b176e623ee59254659e9fa72
MichaelPowell89 <61531405+MichaelPowell89@users.noreply.github.com>
2022-04-25 09:14:15 +1000

    Update AFLE2.jsx

--- src/AFLE2.jsx
+++ src/AFLE2.jsx
@@ -1,13 +1,13 @@
-import afle201 from './public/images/Games/AFLE2/CharlieCameron.png'
-import afle202 from './public/images/Games/AFLE2/HighresScreenshot00204.png'
-import afle203 from './public/images/Games/AFLE2/HighresScreenshot00206.png'
-import afle204 from './public/images/Games/AFLE2/HighresScreenshot00216.png'
-import afle205 from './public/images/Games/AFLE2/HighresScreenshot00218.png'
-import afle206 from './public/images/Games/AFLE2/HighresScreenshot00509.png'
-import afle207 from './public/images/Games/AFLE2/HighresScreenshot00510.png'
-import afle208 from './public/images/Games/AFLE2/HighresScreenshot00523.png'
-import afle209 from './public/images/Games/AFLE2/HighresScreenshot00526.png'
-import afle2010 from './public/images/Games/AFLE2/HighresScreenshot00219.png'
+import afle201 from './images/Games/AFLE2/CharlieCameron.png'
+import afle202 from './images/Games/AFLE2/HighresScreenshot00204.png'
+import afle203 from './images/Games/AFLE2/HighresScreenshot00206.png'
+import afle204 from './images/Games/AFLE2/HighresScreenshot00216.png'
+import afle205 from './images/Games/AFLE2/HighresScreenshot00218.png'
+import afle206 from './images/Games/AFLE2/HighresScreenshot00509.png'
+import afle207 from './images/Games/AFLE2/HighresScreenshot00510.png'
+import afle208 from './images/Games/AFLE2/HighresScreenshot00523.png'
+import afle209 from './images/Games/AFLE2/HighresScreenshot00526.png'
+import afle2010 from './images/Games/AFLE2/HighresScreenshot00219.png'
 
 const AFLE2 = [
   {
