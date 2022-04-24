469e20de076d805664db1a4ab4c3bce86628824e
MichaelPowell89 <61531405+MichaelPowell89@users.noreply.github.com>
2022-04-25 09:10:56 +1000

    Update AFLE2.jsx

--- src/AFLE2.jsx
+++ src/AFLE2.jsx
@@ -2,34 +2,34 @@ import React from "react";
 
 const AFLE2 = [
   {
-    image: '../images/Games/AFLE2/CharlieCameron.png'
+    image: '/portfolio_github/static/media/CharlieCameron.17a61057.png'
   },
   {
-    image: "../images/games/AFLE2/HighresScreenshot00204.png"
+    image: "/portfolio_github/static/media/HighresScreenshot00204.png"
   },
   {
-    image: "../images/games/AFLE2/HighresScreenshot00206.png"
+    image: "/portfolio_github/static/media/HighresScreenshot00206.png"
   },
   {
-    image: "../images/games/AFLE2/HighresScreenshot00216.png"
+    image: "/portfolio_github/static/media/HighresScreenshot00216.png"
   },
   {
-    image: "../images/games/AFLE2/HighresScreenshot00218.png"
+    image: "/portfolio_github/static/media/HighresScreenshot00218.png"
   },
   {
-    image: "../images/games/AFLE2/HighresScreenshot00509.png"
+    image: "/portfolio_github/static/media/HighresScreenshot00509.png"
   },
   {
-    image: "../images/games/AFLE2/HighresScreenshot00510.png"
+    image: "/portfolio_github/static/media/HighresScreenshot00510.png"
   },
   {
-    image: "../images/games/AFLE2/HighresScreenshot00523.png"
+    image: "/portfolio_github/static/media/HighresScreenshot00523.png"
   },
   {
-    image: "../images/games/AFLE2/HighresScreenshot00526.png"
+    image: "/portfolio_github/static/media/HighresScreenshot00526.png"
   },
   {
-    image: "../images/games/AFLE2/HighresScreenshot00219.png"
+    image: "/portfolio_github/static/media/HighresScreenshot00219.png"
   }
 ]
   export default AFLE2;
