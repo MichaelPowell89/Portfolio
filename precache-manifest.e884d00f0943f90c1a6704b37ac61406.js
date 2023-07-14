5a0479a4218d5f5551328c6819c2e0a62bce0dc1
Michael Powell <Michael-Powell>
2023-07-14 14:49:52 +1000

    Updates

---
+++ precache-manifest.e884d00f0943f90c1a6704b37ac61406.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6fc9ba1487ba9c7e794",
+    "url": "/static/css/main.339f5fa0.chunk.css"
+  },
+  {
+    "revision": "d6fc9ba1487ba9c7e794",
+    "url": "/static/js/main.d6fc9ba1.chunk.js"
+  },
+  {
+    "revision": "4e3eec0efd1bea118b1d",
+    "url": "/static/js/1.4e3eec0e.chunk.js"
+  },
+  {
+    "revision": "fdeed3c3ec0611f9c644",
+    "url": "/static/css/2.bb349379.chunk.css"
+  },
+  {
+    "revision": "fdeed3c3ec0611f9c644",
+    "url": "/static/js/2.fdeed3c3.chunk.js"
+  },
+  {
+    "revision": "63100f812c7cb9f3a6d3",
+    "url": "/static/js/runtime~main.63100f81.js"
+  },
+  {
+    "revision": "53f41cbb249858138d0b6c79e2ce7915",
+    "url": "/static/media/controller.53f41cbb.png"
+  },
+  {
+    "revision": "88fccf04af5dcbe78b90387c7a019b75",
+    "url": "/static/media/Xbox.88fccf04.png"
+  },
+  {
+    "revision": "30faa35024eb00c9b3ad62a7a39d68f9",
+    "url": "/static/media/Steam.30faa350.png"
+  },
+  {
+    "revision": "e2532f0a5cc0bee8ca3679ef3b150ca7",
+    "url": "/static/media/websiteWhite.e2532f0a.png"
+  },
+  {
+    "revision": "47305402a5e102588c9946a65939438a",
+    "url": "/static/media/man.47305402.png"
+  },
+  {
+    "revision": "26c94a3d5ec822e6d180ec22009cfb1f",
+    "url": "/static/media/email.26c94a3d.png"
+  },
+  {
+    "revision": "9dfee6e34431a682e1b5880cf861666b",
+    "url": "/static/media/Michael_Powell_CV_GameDesign.9dfee6e3.pdf"
+  },
+  {
+    "revision": "6af8a012de1b5275743a5182c8c3654b",
+    "url": "/static/media/appleWhite.6af8a012.png"
+  },
+  {
+    "revision": "356d05c297ed031f7db6ca914c04ea2a",
+    "url": "/static/media/LogoAsset.356d05c2.png"
+  },
+  {
+    "revision": "7472fa266c22e19f77a9624d45044f08",
+    "url": "/static/media/UnrealEngine.7472fa26.png"
+  },
+  {
+    "revision": "edf343dca51b4ba7400efa7edbf1cdb7",
+    "url": "/static/media/c++.edf343dc.png"
+  },
+  {
+    "revision": "2028cfa9095f6fbae2c788b9626e0544",
+    "url": "/static/media/unity.2028cfa9.png"
+  },
+  {
+    "revision": "2cfe978f6f2a54819103377f91e7842a",
+    "url": "/static/media/Photoshop.2cfe978f.png"
+  },
+  {
+    "revision": "69d3fbd035deee2d55c9a4ff32f53c10",
+    "url": "/static/media/Illustrator.69d3fbd0.png"
+  },
+  {
+    "revision": "eb6e5212ee99eb162faff403d683d33e",
+    "url": "/static/media/XD.eb6e5212.png"
+  },
+  {
+    "revision": "611583b8a0917985410b34f5f1b76ade",
+    "url": "/static/media/Maya.611583b8.png"
+  },
+  {
+    "revision": "f81a6f373c244b1f70f4b7402b5ab372",
+    "url": "/static/media/React.f81a6f37.png"
+  },
+  {
+    "revision": "a0e2387dd04e337f5c36da7821b7cce8",
+    "url": "/index.html"
+  }
+];
