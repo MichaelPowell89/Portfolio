6d2bb4a8cb4ed64afd9985c15f461722764f05c1
Michael Powell <Michael-Powell>
2023-07-25 17:27:48 +1000

    Fix

--- src/views/Web.jsx
+++ src/views/Web.jsx
@@ -1,28 +1,33 @@
 import React from "react";
 import workExamples from "../workExamples";
-import { Card, Container } from "react-bootstrap";
+import { Card, Container, Row } from "react-bootstrap";
 
 const Web = () => {
+  const mobile = window.innerWidth < 768;
+  
   const generateCard = (index) => {
     const example = workExamples[index];
     return (
-      <>
-      <div style={{flex: 1}}/>
-      <Card key={index}>
-      <Card.Body href={example.link}>
-        <Card.Img src={example.image} alt={example.title}/>
-      </Card.Body>
-        <Card.Body>
-          <Card.Title>
-            {example.title} - {example.ReleaseYear}
-          </Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            {example.position}
-          </Card.Subtitle>
-          <Card.Text>{example.description}</Card.Text>
-        </Card.Body>
-      </Card>
-      </>
+      <div style={{flex: 1}}>
+        <Card key={index} className={mobile ? "" : "h-100"}>
+          <Card.Body href={example.link}>
+            <Card.Img
+              src={example.image}
+              alt={example.title}
+              style={{ borderRadius: "5% 5% 0 0" }}
+            />
+          </Card.Body>
+          <Card.Body>
+            <Card.Title>
+              {example.title} - {example.ReleaseYear}
+            </Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              {example.position}
+            </Card.Subtitle>
+            <Card.Text>{example.description}</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
     );
   };
 
@@ -30,7 +35,9 @@ const Web = () => {
     <>
       <div className="overlay" />
       <Container className="ContentPage webPage">
-        {[7, 8, 9].map((index) => generateCard(index))}
+        <Row>
+          {[7, 8, 9].map((index) => generateCard(index))}
+        </Row>
       </Container>
     </>
   );
