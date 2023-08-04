926a239312bd1912537c56776511c2d817f3c230
Michael Powell <Michael-Powell>
2023-08-04 15:25:34 +1000

    Test

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,26 +2,16 @@ import React from "react";
 import { SRLWrapper } from "simple-react-lightbox";
 
 function Content(props) {
-  const blurDivs = document.querySelectorAll(".blur-load");
-  blurDivs.forEach((div) => {
-    const img = div.querySelector("img");
-
-    function loaded() {
-      div.classList.add("loaded");
-    }
-
-    if (img.complete) {
-      loaded();
-    } else {
-      img.addEventListener("load", loaded);
-    }
-  });
-  
   return (
     <SRLWrapper>
       <div className="term">
-      <div className="blur-load" style={{ backgroundImage: `url(${props.blurImage})` }}>
-          <img className="imageThumbnail" src={props.image} alt="" />
+        <div className="blur-load">
+          <img
+            className="imageThumbnail"
+            src={props.image}
+            alt=""
+            loading="lazy" // Add the lazy loading attribute here
+          />
         </div>
       </div>
     </SRLWrapper>
