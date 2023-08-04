6d4244166afc353bd5cde00ad8db4d74a34615b0
Michael Powell <Michael-Powell>
2023-08-04 13:29:27 +1000

    temp

--- src/views/SinglePage.jsx
+++ src/views/SinglePage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Home from "./Home.jsx";
 import Game from "./Game.jsx";
 import Web from "./Web.jsx";
@@ -8,67 +7,32 @@ import Contact from "./Contact.jsx";
 
 const SinglePage = () => {
   const mobile = window.innerWidth < 992;
-  const [shouldAnimate, setShouldAnimate] = useState(false);
 
-  useEffect(() => {
-    // Introduce a delay before triggering initial animation
-    const timer = setTimeout(() => {
-      setShouldAnimate(true);
-    }, 300);
+  const sectionComponents = {
+    game: Game,
+    web: Web,
+    about: About,
+    contact: Contact,
+  };
 
-    return () => clearTimeout(timer);
-  }, []);
-
-  const [lazyComponents, setLazyComponents] = useState({
-    game: null,
-    web: null,
-    about: null,
-    contact: null,
-  });
+  const [activeSection, setActiveSection] = useState("home");
 
   useEffect(() => {
     const observer = new IntersectionObserver(
-      async (entries) => {
+      (entries) => {
         for (const entry of entries) {
           if (entry.isIntersecting) {
             const sectionId = entry.target.getAttribute("id");
-            switch (sectionId) {
-              case "home":
-                break;
-              case "game":
-                setLazyComponents((prevComponents) => ({
-                  ...prevComponents,
-                  game: Game,
-                }));
-                break;
-              case "web":
-                setLazyComponents((prevComponents) => ({
-                  ...prevComponents,
-                  web: Web,
-                }));
-                break;
-              case "about":
-                setLazyComponents((prevComponents) => ({
-                  ...prevComponents,
-                  about: About,
-                }));
-                break;
-              case "contact":
-                setLazyComponents((prevComponents) => ({
-                  ...prevComponents,
-                  contact: Contact,
-                }));
-                break;
-              default:
-                break;
-            }
+            setTimeout(() => {
+              setActiveSection(sectionId);
+            }, 500); 
           }
         }
       },
       {
         root: null,
         rootMargin: "0px",
-        threshold: 0.8,
+        threshold: 0.2,
       }
     );
 
@@ -84,42 +48,73 @@ const SinglePage = () => {
     };
   }, []);
 
-  const renderLazyComponent = (Component, slideFromLeft) => {
-    const enterClass = slideFromLeft ? "slide-enter-left" : "slide-enter";
-    const enterActiveClass = slideFromLeft
-      ? "slide-enter-active-left"
-      : "slide-enter-active";
-
-    return (
-      <CSSTransition
-        in={!!Component}
-        timeout={500}
-        classNames={{ enter: enterClass, enterActive: enterActiveClass }}
-      >
-        <div>
-          {Component ? <Component /> : null}
-        </div>
-      </CSSTransition>
-    );
-  };
-
   return (
     <>
-      <section id="home" style={{ height: "100vh" }}>
-        <Home />
-      </section>
-      <section id="game" className={`backgroundVariant slide-in`}>
-        {renderLazyComponent(lazyComponents.game)}
-      </section>
-      <section id="web">
-        {renderLazyComponent(lazyComponents.web, true)}
-      </section>
-      <section id="about" className={`backgroundVariant slide-in`}>
-        {renderLazyComponent(lazyComponents.about)}
-      </section>
-      <section id="contact">
-        {renderLazyComponent(lazyComponents.contact, true)}
-      </section>
+      {mobile ? (
+        <>
+          <section id="home" style={{ height: "100vh" }}>
+            <Home />
+          </section>
+          <section id="game" className={`backgroundVariant`}>
+              <Game />
+          </section>
+          <section id="web">
+              <Web />
+          </section>
+          <section id="about" className={`backgroundVariant`}>
+              <About />
+          </section>
+          <section id="contact">
+              <Contact />
+          </section>
+        </>
+      ) : (
+        <>
+          <section id="home" style={{ height: "100vh" }}>
+            <Home />
+          </section>
+          <section id="game" className={`backgroundVariant`}>
+            <div
+              className={`offScreenRight ${
+                activeSection === "game" ? "inFocus" : ""
+              }`}
+              style={{ transition: "transform 500ms ease-in-out" }}
+            >
+              <Game />
+            </div>
+          </section>
+          <section id="web">
+            <div
+              className={`offScreenLeft ${
+                activeSection === "web" ? "inFocus" : ""
+              }`}
+              style={{ transition: "transform 500ms ease-in-out" }}
+            >
+              <Web />
+            </div>
+          </section>
+          <section id="about" className={`backgroundVariant`}>
+            <div
+              className={`offScreenRight ${
+                activeSection === "about" ? "inFocus" : ""
+              }`}
+              style={{ transition: "transform 500ms ease-in-out" }}
+            >
+              <About />
+            </div>
+          </section>
+          <section id="contact">
+            <div
+              className={`offScreenLeft ${
+                activeSection === "contact" ? "inFocus" : ""
+              }`}
+              style={{ transition: "transform 500ms ease-in-out" }}
+            >
+              <Contact />
+            </div>
+          </section>
+        </>
+      )}
     </>
   );
 };
